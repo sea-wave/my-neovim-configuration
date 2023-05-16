cc79bfaf9fa397568b6df390952fb38c931bb6d5
Gal Gur-Arie <gal.ga75@gmail.com>
2023-05-16 12:43:55 +0300

    Adding colorscheme support for the 256 Apple Terminal Application

--- lua/user/core/colorscheme.lua
+++ lua/user/core/colorscheme.lua
@@ -5,3 +5,16 @@ if not status then
   print("Colorscheme not found!") -- print error if colorscheme not installed
   return
 end
+
+if os.getenv("TERM_PROGRAM") == "Apple_Terminal" then
+  local callStatusTerm, _ = pcall(vim.cmd, "set notermguicolors")
+  if not callStatusTerm then
+    print("failed to set notermguicolors") -- print error if colorscheme not installed
+    return
+  end
+  local statusTermColor, _ = pcall(vim.cmd, "colorscheme gruvbox-material")
+  if not statusTermColor then
+    print("Colorscheme not found!") -- print error if colorscheme not installed
+    return
+  end
+end
